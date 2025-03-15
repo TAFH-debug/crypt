4c5e25450024c25ede452f47fef62bf1d9539605
TAFH-debug <talim2007@mail.ru>
2025-03-15 10:41:22 +0500

    Basic Tauri + SvelteKit + TypeScript + TailwindCSS setup.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Password from "./components/password.svelte";
+
+  let name = $state("");
+</script>
+
+<main class="min-h-screen bg-gray-700">
+  <h1 class="text-center w-full text-5xl text-white py-20">
+    Password Manager
+  </h1>
+  <div class="w-4/5 flex items-center flex-col m-auto">
+    <Password service={"Discord"} password={"123456789"}/>
+  </div>
+</main>
+
+<style>
+
+</style>
